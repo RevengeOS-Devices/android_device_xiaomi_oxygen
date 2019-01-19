8ade39ab130fd949f3a3821905f268a0def7e2bf
Sarthak Malla <sarthakmalla17@gmail.com>
2019-01-19 06:03:07 +0000

    RR-IFY

--- device.mk
+++ device.mk
@@ -68,3 +68,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017.xml:system/etc/sysconfig/pixel_2017.xml \
     $(LOCAL_PATH)/pixelstyle/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
     $(LOCAL_PATH)/pixelstyle/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml
+
+#RR OFFICIAL
+export RR_BUILDTYPE=Official
